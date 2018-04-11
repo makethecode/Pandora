ff633cdd191d86b50ae706d9e78ec41e3311fdf5
dyw <1049237372@qq.com>
2018-04-11 22:36:52 +0800

    'dyw'

--- app/src/main/java/com/dengyangwu/codekiller/pandora/MenuActivity.java
+++ app/src/main/java/com/dengyangwu/codekiller/pandora/MenuActivity.java
@@ -1,6 +1,8 @@
 package com.dengyangwu.codekiller.pandora;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -9,7 +11,11 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -25,80 +31,81 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.baidu.aip.imageclassify.AipImageClassify;
+
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 import static android.app.Activity.RESULT_OK;
 
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static int REQUEST_CAMERA_1 = 1;
-    private static int REQUEST_CAMERA_2 = 2;
-    private Uri fileUri;
+
+    //设置APPID/AK/SK
+    public static final String APP_ID = "11077051";
+    public static final String API_KEY = "qt63QRAaZ2xcW5hyD1TjncqZ";
+    public static final String SECRET_KEY = "2r12GKDgC8obqRzsrRZeyMcoPvHAK4vq";
+
+    private static final int MY_PERMISSION_REQUEST_CODE = 10000;
+    private static int REQUEST_CAMERA_1 = 1;//相机小图 适合做头像
+    private static int REQUEST_CAMERA_2 = 2;//相机原图 比较大
     private String mFilePath;
     private ImageView lv;
+    private TextView tv_msg;
+    private AipImageClassify classify;
+    private Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         init();
-
-
-        mFilePath = Environment.getExternalStorageDirectory().getPath();// 获取SD卡路径
-        mFilePath = mFilePath + "/" + "tempdf.png";// 指定路径
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (Build.VERSION.SDK_INT >= 23) {
-                    int checkCallPhonePermission = ContextCompat.checkSelfPermission(MenuActivity.this, Manifest.permission.CAMERA);
-                    if(checkCallPhonePermission != PackageManager.PERMISSION_GRANTED){
-                        ActivityCompat.requestPermissions(MenuActivity.this,new String[]{Manifest.permission.CAMERA},222);
-                        return;
-                    }else{
-                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);// 启动系统相机
-                       // Uri photoUri = Uri.fromFile(new File(mFilePath)); // 传递路径
-                       // intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);// 更改系统默认存储路径
-                        startActivityForResult(intent,REQUEST_CAMERA_1);
-                    }
-
-                } else {
-
-                    Snackbar.make(view, mFilePath, Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-
-
-
-
+                requestGranted();
+                //Snackbar.make(view, mFilePath, Snackbar.LENGTH_LONG).setAction("Action", null).show();
 
             }
         });
 
+
+    }
+
+
+    public  void init(){
+
+        handler=new MyHandler(this);
+        classify=new AipImageClassify(APP_ID,API_KEY,SECRET_KEY);
+        mFilePath = Environment.getExternalStorageDirectory().getPath();// 获取SD卡路径
+        mFilePath = mFilePath + "/" + "tempdf.png";// 指定路径
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-    }
+        tv_msg=findViewById(R.id.tv_mes);
 
-
-    public  void init(){
         lv=findViewById(R.id.Iv);
     }
     @Override
@@ -172,6 +179,8 @@ public class MenuActivity extends AppCompatActivity
                     fis = new FileInputStream(mFilePath); // 根据路径获取数据
                     Bitmap bitmap = BitmapFactory.decodeStream(fis);
                     lv.setImageBitmap(bitmap);// 显示图片
+                    new Thread(networkTask).start();
+
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 } finally {
@@ -184,4 +193,138 @@ public class MenuActivity extends AppCompatActivity
             }
         }
     }
+    private void requestGranted(){
+        boolean isAllGranted = checkPermissionAllGranted(
+                new String[] {
+
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                }
+        );
+        // 如果这3个权限全都拥有, 则直接执行备份代码
+        if (isAllGranted) {
+            //doBackup();
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);// 启动系统相机
+            Uri photoUri = Uri.fromFile(new File(mFilePath)); // 传递路径
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);// 更改系统默认存储路径
+            startActivityForResult(intent,REQUEST_CAMERA_2);
+            return;
+        }
+
+        /**
+         * 第 2 步: 请求权限
+         */
+        // 一次请求多个权限, 如果其他有权限是已经授予的将会自动忽略掉
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] {
+
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                },
+                MY_PERMISSION_REQUEST_CODE
+        );
+    }
+    private boolean checkPermissionAllGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                // 只要有一个权限没有被授予, 则直接返回 false
+                return false;
+            }
+        }
+        return true;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MY_PERMISSION_REQUEST_CODE) {
+            boolean isAllGranted = true;
+
+            // 判断是否所有的权限都已经授予了
+            for (int grant : grantResults) {
+                if (grant != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false;
+                    break;
+                }
+            }
+
+            if (isAllGranted) {
+                // 如果所有的权限都授予了, 则执行备份代码
+                //doBackup();
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);// 启动系统相机
+                Uri photoUri = Uri.fromFile(new File(mFilePath)); // 传递路径
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);// 更改系统默认存储路径
+                startActivityForResult(intent,REQUEST_CAMERA_2);
+
+            } else {
+                // 弹出对话框告诉用户需要权限的原因, 并引导用户去应用权限管理中手动打开权限按钮
+                openAppDetails();
+            }
+        }
+    }
+    private void openAppDetails() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("使用照相机需要访问 “Camera” 和 “外部存储器”，请到 “应用信息 -> 权限” 中授予！");
+        builder.setPositiveButton("去手动授权", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("取消", null);
+        builder.show();
+    }
+
+    private void getAnimalMes(){
+
+
+    }
+
+    //开辟百度API线路连接
+    Runnable networkTask=new Runnable() {
+        @Override
+        public void run() {
+
+            HashMap<String,String> options=new HashMap<String,String>();
+            options.put("top_num","3");
+            JSONObject res=classify.animalDetect(mFilePath,options);
+            Message msg=new Message();
+            Bundle data=new Bundle();
+            data.putString("value",res.toString());
+            msg.setData(data);
+            handler.sendMessage(msg);
+        }
+    };
+
+    static  class MyHandler extends Handler{
+
+        WeakReference<MenuActivity> menuActivityWeakReference;
+        public MyHandler(MenuActivity menuActivity){
+            menuActivityWeakReference=new WeakReference<MenuActivity>(menuActivity);
+        }
+        @Override
+        public void handleMessage(Message msg){
+            MenuActivity menuActivity1=menuActivityWeakReference.get();
+            Bundle data=msg.getData();
+            String val=data.getString("value").toString();
+            Log.i("mylog", "请求结果为-->" + val);
+            menuActivity1.tv_msg.setText(val);
+        }
+
+    }
+
 }
